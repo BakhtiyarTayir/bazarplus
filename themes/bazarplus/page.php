b6af2b80d7254d80c889f6d9f008f2c5676bae7d
Jaloliddin-Fozilov <jaloliddinish@gmail.com>
2022-07-23 11:08:03 +0500

    page.php complated

--- themes/bazarplus/page.php
+++ themes/bazarplus/page.php
@@ -14,24 +14,23 @@
 
 get_header();
 ?>
+		<div class="container">
+			<?php
+				while ( have_posts() ) :
+					the_post();
 
-	<main id="primary" class="site-main page-content header-clear-medium">
+					get_template_part( 'template-parts/content', 'page' );
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) :
+						comments_template();
+					endif;
 
-			get_template_part( 'template-parts/content', 'page' );
+				endwhile; // End of the loop.
+			?>
+		</div>
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
-
-	</main><!-- #main -->
+	</div><!-- #div -->
 
 <?php
 get_footer();
--- themes/bazarplus/template-parts/content-page.php
+++ themes/bazarplus/template-parts/content-page.php
@@ -9,46 +9,11 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
-
-	<?php bazarplus_post_thumbnail(); ?>
-
-	<div class="entry-content">
-		<?php
-		the_content();
-
-		wp_link_pages(
-			array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'bazarplus' ),
-				'after'  => '</div>',
-			)
-		);
-		?>
-	</div><!-- .entry-content -->
-
-	<?php if ( get_edit_post_link() ) : ?>
-		<footer class="entry-footer">
-			<?php
-			edit_post_link(
-				sprintf(
-					wp_kses(
-						/* translators: %s: Name of current post. Only visible to screen readers */
-						__( 'Edit <span class="screen-reader-text">%s</span>', 'bazarplus' ),
-						array(
-							'span' => array(
-								'class' => array(),
-							),
-						)
-					),
-					wp_kses_post( get_the_title() )
-				),
-				'<span class="edit-link">',
-				'</span>'
-			);
-			?>
-		</footer><!-- .entry-footer -->
-	<?php endif; ?>
-</article><!-- #post-<?php the_ID(); ?> -->
+<h1 class="page-title"><?php the_title(); ?></h1>
+<div class="page-main-content">
+	<div class="wpb_text_column">
+		<div class="wpb_wrapper">
+			<?php the_content(); ?>
+		</div>
+	</div>
+</div>
